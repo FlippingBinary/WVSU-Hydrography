1811577a0b26f68992393d5419920d15956507f1
FlippingBinary <35066367+FlippingBinary@users.noreply.github.com>
2020-07-22 11:57:01 -0400

    Create boilerplate frontend

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Front end</title>
+      </Head>
+      <div>Hello, World!</div>
+    </>
+  )
+}
