b67955fcedd96691b8751de53b7b9eb71d912199
FlippingBinary <35066367+FlippingBinary@users.noreply.github.com>
2020-11-23 12:33:11 -0500

    Fix relative links

--- frontend/pages/components/nav.js
+++ frontend/pages/components/nav.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
-  { href: '../precipitation', label: 'Precipitation' },
-  { href: '../evaporation', label: 'Evaporation' },
-  { href:'../index' , label:'Home'}
+  { href: '/precipitation', label: 'Precipitation' },
+  { href: '/evaporation', label: 'Evaporation' },
+  { href:'/' , label:'Home'}
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
   return link
