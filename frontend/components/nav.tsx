7d12334587384f1a34f14589de2a4fb6f7999277
FlippingBinary <35066367+FlippingBinary@users.noreply.github.com>
2020-11-23 13:11:00 -0500

    Rearrange Nav links

--- frontend/components/nav.tsx
+++ frontend/components/nav.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
-  { href: '/precipitation', label: 'Precipitation' },
+  { href:'/' , label:'Home'},
   { href: '/evaporation', label: 'Evaporation' },
-  { href:'/' , label:'Home'}
+  { href: '/precipitation', label: 'Precipitation' }
 ]
 
 const Nav = ({ path }: {path:string}) => (
